272343fba32b9c7a88fe283d8ad0f429ec05a95e
Lim JunWoo <106497516+ceo7913@users.noreply.github.com>
2023-06-06 01:32:51 +0900

    Update 곱셈.js

--- 백준/Bronze/2588. 곱셈/곱셈.js
+++ 백준/Bronze/2588. 곱셈/곱셈.js
@@ -12,3 +12,18 @@ console.log(a * digit1);
 console.log(a * digit10);
 console.log(a * digit100);
 console.log(a * b);
+
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// let a = input[0];
+// let b = input[1];
+
+// digit1 = b[2];
+// digit10 = b[1];
+// digit100 = b[0];
+
+// console.log(Number(a) * Number(digit1));
+// console.log(Number(a) * Number(digit10));
+// console.log(Number(a) * Number(digit100));
+// console.log(Number(a) * Number(b));
