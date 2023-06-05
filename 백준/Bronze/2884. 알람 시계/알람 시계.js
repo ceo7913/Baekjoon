a4c86f4abb6620867e1bce71019e89cd0d965c42
Lim JunWoo <106497516+ceo7913@users.noreply.github.com>
2023-06-06 03:37:11 +0900

    Update 알람 시계.js

--- 백준/Bronze/2884. 알람 시계/알람 시계.js
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -2,6 +2,8 @@ let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split("\n")
 
 let [H, M] = input[0].split(' ').map(Number);
+// let H = Number(input[0].split(' ')[0]);
+// let M = Number(input[1].split(' ')[1]);
 
 M -= 45;
 
