d2a5468733dd81c8d6e5a2167d98184984a91266
Lim JunWoo <106497516+ceo7913@users.noreply.github.com>
2023-06-06 02:47:28 +0900

    Update 시험 성적.js

--- 백준/Bronze/9498. 시험 성적/시험 성적.js
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -13,3 +13,29 @@ function checkRecord(a) {
 }
 
 checkRecord(data);
+
+/*
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(" ");
+
+
+let MyScroe = Number(input[0]);
+
+let checkRecord = (i) => {
+  let score = ""
+  if (i >= 90 && i <= 100) {
+    score = "A"
+  } else if (i >= 80 && i <= 89) {
+    score = "B"
+  } else if (i >= 70 && i <= 79) {
+    score = "C"
+  } else if (i >= 60 && i <= 69) {
+    score = "D"
+  } else {
+    score = "F"
+  }
+  return score;
+}
+
+console.log(checkRecord(MyScroe))
+*/
