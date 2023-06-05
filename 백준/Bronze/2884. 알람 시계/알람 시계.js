1c20131086eac570c92c9c5b92d92489d2a57e5a
Lim JunWoo <106497516+ceo7913@users.noreply.github.com>
2023-06-06 03:28:24 +0900

    [Bronze III] Title: 알람 시계, Time: 152 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -0,0 +1,17 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split("\n")
+
+let [H, M] = input[0].split(' ').map(Number);
+
+M -= 45;
+
+if (M < 0) { 
+  M += 60; 
+  H -= 1; 
+
+  if (H < 0) { 
+    H = 23; 
+  }
+}
+
+console.log(H + " " + M);
