2d5ac9e8638327bf8414919ee0915806b9d567d2
Lim JunWoo <106497516+ceo7913@users.noreply.github.com>
2023-06-06 22:07:55 +0900

    Update 오븐 시계.js

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.js
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const input = fs.readFileSync('/dev/stdin','utf8').toString().split('\n');
 
 const [A, B] = input[0].split(' ').map(Number);
 const C = Number(input[1]);
@@ -9,3 +9,15 @@ const H = Math.floor(totalM / 60);
 const M = totalM % 60;
 
 console.log(`${H} ${M}`);
+
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split("\n");
+
+// let [A,B] = input[0].split(" ").map(Number);
+// let C = Number(input[1]);
+
+// let totalM = (A * 60 + B + C) % 1440;
+// let H = parseInt(totalM / 60);
+// let M = totalM % 60;
+
+// console.log(H + " " + M)
