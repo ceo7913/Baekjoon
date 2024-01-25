5098e6004bc45f8307bfa4dfa4644780e72ff2bf
Lim JunWoo <106497516+ceo7913@users.noreply.github.com>
2024-01-25 15:24:09 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 188 ms, Memory: 10172 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.js
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.js
@@ -3,51 +3,29 @@ let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 const height = Number(input[0].split(" ")[0]);
 const width = Number(input[0].split(" ")[1]);
 const chessboard = [];
-let answer = 0;
+
 for (let i = 1; i <= height; i++) {
   input[i] = input[i].slice(0, width);
   chessboard.push(input[i].split(""));
 }
 
-const whiteFirstPaint = (i, j) => {
+const countPaint = (i, j, startColor) => {
   let count = 0;
-  for (let w = i; w <= i + 7; w++) {
-    for (let h = j; h <= j + 7; h++) {
-      if ((w % 2 === 0 && h % 2 === 1) || (w % 2 === 1 && h % 2 === 0)) {
-        count = chessboard[h][w] === "B" ? count : count + 1;
-      } else if ((w % 2 === 0 && h % 2 === 0) || (w % 2 === 1 && h % 2 === 1)) {
-        count = chessboard[h][w] === "W" ? count : count + 1;
-      }
+  for (let w = i; w < i + 8; w++) {
+    for (let h = j; h < j + 8; h++) {
+      const isSameColor = (w + h) % 2 === startColor;
+      count += chessboard[h][w] !== (isSameColor ? "W" : "B");
     }
   }
   return count;
 };
 
-const blackFirstPaint = (i, j) => {
-  let count = 0;
-  for (let w = i; w <= i + 7; w++) {
-    for (let h = j; h <= j + 7; h++) {
-      if ((w % 2 === 0 && h % 2 === 1) || (w % 2 === 1 && h % 2 === 0)) {
-        count = chessboard[h][w] === "W" ? count : count + 1;
-      } else if ((w % 2 === 0 && h % 2 === 0) || (w % 2 === 1 && h % 2 === 1)) {
-        count = chessboard[h][w] === "B" ? count : count + 1;
-      }
-    }
-  }
-  return count;
-};
+let answer = Infinity;
 
 for (let i = 0; i + 7 < width; i++) {
   for (let j = 0; j + 7 < height; j++) {
-    const whiteFirst = whiteFirstPaint(i, j);
-    const blackFirst = blackFirstPaint(i, j);
-    if (i === 0 && j === 0) {
-      answer = Math.min(whiteFirst, blackFirst);
-    } else {
-      if (Math.min(whiteFirst, blackFirst) < answer) {
-        answer = Math.min(whiteFirst, blackFirst);
-      }
-    }
+    answer = Math.min(answer, countPaint(i, j, 0), countPaint(i, j, 1));
   }
 }
+
 console.log(answer);
